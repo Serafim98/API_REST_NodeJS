57cbd61d73b96cd14d277189a32ed50ec99b0b9d
Luis Serafim <fernandodss30@hotmail.com>
2022-01-11 21:51:20 -0300

    Inclusão e Listagem simples de atendimentos

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,11 +1,15 @@
 const Atendimento = require('../models/atendimentos')
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send("Você está na rota de atendimentos e está realizando um GET"));
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res);
+    });
+
+    
 
     app.post('/atendimentos', (req, res)=> {
         const atendimento = req.body;
         
-        Atendimento.adiciona(atendimento);
-        res.send('Você está na rota de atendimentos e está realizando um POST')
+        Atendimento.adiciona(atendimento,  res);
+       
     })
 }
