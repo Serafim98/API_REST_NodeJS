d976e30d839768e9d5597ecede6b5995e48ebdcb
Luis Serafim <fernandodss30@hotmail.com>
2022-01-20 21:51:43 -0300

    Tratamento de erro na rota pets

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,9 +1,21 @@
 const fs = require('fs')
+const path = require('path');
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada)=>{
-    const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
+    const tipo = path.extname(caminho);
+    const tiposValidos = ['jpg', 'png', 'jpeg'];
+    let eValido = tiposValidos.indexOf(tipo.substring(1)) != -1;
 
-    fs.createReadStream(caminho)
-    .pipe(fs.createWriteStream(novoCaminho))
-    .on('finish', () => callbackImagemCriada(novoCaminho))
+    if(eValido){
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+        fs.createReadStream(caminho)
+        .pipe(fs.createWriteStream(novoCaminho))
+        .on('finish', () => callbackImagemCriada(false, novoCaminho))
+    }
+    else{
+        const erro = "Tipo é inválido"
+        console.log("Tipo Inválido")
+        callbackImagemCriada(erro);
+    }
+    
 }
--- models/pets.js
+++ models/pets.js
@@ -5,8 +5,12 @@ class Pet {
     adiciona(pet, res) {
         const query = 'INSERT INTO Pets SET?'
 
-        uploadDeArquivos(pet.imagem, pet.nome, (novoCaminho) => {
-            const novoPet = {nome: pet.nome, imagem: novoCaminho}
+        uploadDeArquivos(pet.imagem, pet.nome, (erro, novoCaminho) => {
+            if(erro){
+                res.status(400).json({erro})
+            }
+            else{
+                const novoPet = {nome: pet.nome, imagem: novoCaminho}
             conexao.query(query, novoPet, erro => {
                 if (erro) {
                     console.log(erro)
@@ -14,8 +18,10 @@ class Pet {
                 } else {
                     res.status(200).json(novoPet)
                 }
+            })
+            }
         })
-    })
-}
+    }
+            
 }
 module.exports = new Pet()
